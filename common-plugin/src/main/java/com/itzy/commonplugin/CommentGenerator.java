b31b253aacf74f8265044390acb955c5d4f32b75
zhangyu <13100650510@163.com>
2019-08-01 18:00:43 +0800

    feat:新增公共插件类

---
+++ common-plugin/src/main/java/com/itzy/commonplugin/CommentGenerator.java
@@ -0,0 +1,30 @@
+package com.itzy.commonplugin;
+
+/**
+ * @Author: ZY
+ * @Date: 2019/8/1 17:57
+ * @Version 1.0
+ */
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * Created By tangbj On 2019/6/19
+ * Description: 实体类中字段增加注释
+ */
+public class CommentGenerator extends DefaultCommentGenerator {
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        super.addFieldComment(field, introspectedTable, introspectedColumn);
+        if (introspectedColumn.getRemarks() != null && !introspectedColumn.getRemarks().equals("")) {
+            field.addJavaDocLine("/**");
+            field.addJavaDocLine(" * " + introspectedColumn.getRemarks());
+            field.addJavaDocLine(" */");
+        }
+    }
+
+}
