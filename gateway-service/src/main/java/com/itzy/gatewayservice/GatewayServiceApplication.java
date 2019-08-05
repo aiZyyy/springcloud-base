54c854d084929b53b324f5f6b728559a9d11da2c
zhangyu <13100650510@163.com>
2019-08-05 10:14:24 +0800

    fix:修改配置,与注册中心联合

--- gateway-service/src/main/java/com/itzy/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/itzy/gatewayservice/GatewayServiceApplication.java
@@ -1,11 +1,10 @@
 package com.itzy.gatewayservice;
 
+import com.itzy.commonservice.annotation.ZyMicroServiceApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringCloudApplication
+@ZyMicroServiceApplication
 public class GatewayServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
